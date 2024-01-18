0135757567fe031333b1eeee5c605de98633acea
Sam Levine <samlevine03@gmail.com>
2024-01-18 00:56:15 -0600

    changed comment

--- settings.py
+++ settings.py
@@ -1,2 +1,2 @@
-SUGGEST_THRESHOLD = 0.065  # number from 0.010 to 1.000 (default is 0.065)
+SUGGEST_THRESHOLD = 0.065  # number from 0.01 to 1.00 (default is 0.065)
 DICT_SIZE = 1.00  # number from 0.01 to 1.00 (default is 1.00)
